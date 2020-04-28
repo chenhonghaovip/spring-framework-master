a22e229e540da20e183d05ca98931e6e6eb4a91b
CHH <949206605@qq.com>
2020-04-28 13:23:14 +0800

    commit by chh for doc

--- spring-context/src/test/java/test/aspect/MyApplicationListener.java
+++ spring-context/src/test/java/test/aspect/MyApplicationListener.java
@@ -1,16 +1,16 @@
 package test.aspect;
 
 import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 
 /**
  * @author chenhonghao
  * @date 2020-04-23 16:11
  */
-public class MyApplicationListener implements ApplicationListener<ApplicationEvent> {
+public class MyApplicationListener {
 	@Async
-	@Override
+	@EventListener
 	public void onApplicationEvent(ApplicationEvent event) {
 		System.out.println("MyApplicationListener监听事件"+event);
 	}

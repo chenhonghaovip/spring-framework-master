9d49f4f34259da37352b60eef2c44ef02bd505ce
chenhonghao <949206605@qq.com>
2020-04-29 20:17:46 +0800

    change for gradle version

--- spring-context/src/test/java/test/TestMain.java
+++ spring-context/src/test/java/test/TestMain.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import test.config.AOPConfig;
 import test.dto.Fox;
 
+import java.util.Arrays;
+
 /**
  * @author chenhonghao
  * @date 2020-01-13 20:47
@@ -16,9 +18,9 @@ public class TestMain {
     public void test11(){
         AnnotationConfigApplicationContext an = new AnnotationConfigApplicationContext(AOPConfig.class);
         Fox fox = (Fox) an.getBean("fox");
-//		System.out.println("ioc容器中所有bean实例开始");
-//		Arrays.stream(an.getBeanDefinitionNames()).forEach(System.out::println);
-//		System.out.println("ioc容器中所有bean实例结束");
+		System.out.println("ioc容器中所有bean实例开始");
+		Arrays.stream(an.getBeanDefinitionNames()).forEach(System.out::println);
+		System.out.println("ioc容器中所有bean实例结束");
 		// 容器中保存了组件的代理对象（cglib增强后的对象），这个对象里面保存了详细信息（比如增强器，目标对象）
 		an.publishEvent(new ApplicationEvent("我发布了一个事件"){});
 
